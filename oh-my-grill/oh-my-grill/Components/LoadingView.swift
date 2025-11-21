804540afe7dd8b15e3b6198d4158d54da48b132e
barbaradasilvadapper <barbaradasilvadapper@gmail.com>
2025-11-21 14:57:54 -0300

    loading view

---
+++ oh-my-grill/oh-my-grill/Components/LoadingView.swift
@@ -0,0 +1,31 @@
+//
+//  LoadingView.swift
+//  oh-my-grill
+//
+//  Created by Barbara da Silva Dapper on 21/11/25.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    
+    var body: some View {
+        ZStack(alignment: .center) {
+            //MARK: Fundo
+            Image(.backgroundOut)
+                .resizable()
+                .scaledToFill()
+                .ignoresSafeArea()
+            
+            //MARK: Conteúdo
+            Image(.star)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 118, height: 136)
+                .padding(.bottom, 65)
+        }
+    }
+}
+#Preview {
+    LoadingView()
+}
