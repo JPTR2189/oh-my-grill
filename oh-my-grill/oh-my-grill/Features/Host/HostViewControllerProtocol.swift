1988f9fb71c31a0f7b715492175da4823b473bf1
João Carvalho <joaopedroteixeira2004@gmail.com>
2025-11-21 15:31:00 -0300

    HostView + MP + fix to components

---
+++ oh-my-grill/oh-my-grill/Features/Host/HostViewControllerProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  HostViewControllerProtocol.swift
+//  oh-my-grill
+//
+//  Created by João Pedro Teixeira de Carvalho on 21/11/25.
+//
+
+import Foundation
+
+protocol HostViewControllerProtocol {
+    var transport: any TransportSessionProtocol { get }
+    var password: [String] { get }
+    
+}
