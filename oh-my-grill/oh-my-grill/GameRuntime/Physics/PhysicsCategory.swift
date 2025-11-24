1e7aadb0084494bb088e183d8f4ca5755e376372
João Carvalho <joaopedroteixeira2004@gmail.com>
2025-11-24 14:09:10 -0300

    Adding game stubs from POC

---
+++ oh-my-grill/oh-my-grill/GameRuntime/Physics/PhysicsCategory.swift
@@ -0,0 +1,17 @@
+//
+//  PhysicsCategory.swift
+//  poc-peerConnectivity
+//
+//  Created by João Pedro Teixeira de Carvalho on 10/11/25.
+//
+
+import Foundation
+
+// Physics category - used to set the different hit categories
+public struct PhysicsCategory {
+    static let edge: UInt32 = 1 << 0
+    static let parcel: UInt32 = 1 << 1
+    static let sensorRight: UInt32 = 1 << 2
+    static let sensorLeft: UInt32 = 1 << 3
+    static let obstacle: UInt32 = 1 << 4
+}
