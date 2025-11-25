35875bc48b0e374df2c7d6d05d46196bd3c09ced
João Carvalho <joaopedroteixeira2004@gmail.com>
2025-11-25 17:14:29 -0300

    updating GameSession to support transport protocol

--- oh-my-grill/oh-my-grill/Features/AssignRole/Host/HostAssignViewModel.swift
+++ oh-my-grill/oh-my-grill/Features/AssignRole/Host/HostAssignViewModel.swift
@@ -78,10 +78,10 @@ final class HostAssignViewModel: HostAssignViewModelProtocol {
         transport.send(message)
 
         print("Initializing gameSession on host, should start the game")
-//        GameSession(
-//            transport: transport,
-//            config: payload
-//        )
+        self.gameSession =  GameSession(
+            transport: transport,
+            config: payload
+        )
     }
 }
 
