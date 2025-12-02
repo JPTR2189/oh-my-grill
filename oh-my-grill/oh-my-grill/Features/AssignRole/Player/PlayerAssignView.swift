9fa3154cd071ab3cf1a9d7267a728e4f8cda9eca
barbaradasilvadapper <barbaradasilvadapper@gmail.com>
2025-12-02 17:27:32 -0300

    fixing aasign

--- oh-my-grill/oh-my-grill/Features/AssignRole/Player/PlayerAssignView.swift
+++ oh-my-grill/oh-my-grill/Features/AssignRole/Player/PlayerAssignView.swift
@@ -72,8 +72,7 @@ struct PlayerAssignView: View {
             .navigationBarBackButtonHidden(true)
             .fullScreenCover(isPresented: $nextView) {
                 if let session = vm.gameSession {
-                    //ChefView(vm: ChefViewModel(session: session))
-                    GrillMiniGameView(vm: GrillMiniGameViewModel(session: session))
+                    ChefView(vm: ChefViewModel(session: session))
                 }
             }
         }
