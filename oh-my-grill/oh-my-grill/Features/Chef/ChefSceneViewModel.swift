627b0523a365beb0359fb992bc51db55f8f3e4f0
= <=>
2025-12-05 17:08:31 -0300

    fix: Removes function addDropShadow

--- oh-my-grill/oh-my-grill/Features/Chef/ChefSceneViewModel.swift
+++ oh-my-grill/oh-my-grill/Features/Chef/ChefSceneViewModel.swift
@@ -19,7 +19,7 @@ class ChefSceneViewModel {
 
     private var cont = 0
 
-    let opcoes = ["burger-cheesed", "tomato-sliced", "lettuce-sliced", "potato-fried", "bun-top",  "bun-bottom"]
+    let opcoes = ["burger-cheesed-shade", "tomato-sliced-shade", "lettuce-sliced-shade", "potato-fried-shade", "bun-top-shade",  "bun-bottom-shade"]
 
 
     func criarPratoRedondo(isPotato: Bool = false) {
@@ -67,7 +67,7 @@ class ChefSceneViewModel {
     
     func componenteIngrediente(nome: String, posicaoIngrediente: CGFloat) {
         
-        let tamanho = nome == "potato-fried" ? CGSize(width: 100  , height: 65) : CGSize(width: 65 , height: 65)
+        let tamanho = nome == "potato-fried-shade" ? CGSize(width: 125  , height: 90) : CGSize(width: 90 , height: 90)
         
         let ingrediente = SKSpriteNode(texture: SKTexture(imageNamed: nome), color: .clear, size: tamanho)
         ingrediente.position = CGPoint(x: scene.frame.minX + posicaoIngrediente, y: scene.frame.minY + 50)
