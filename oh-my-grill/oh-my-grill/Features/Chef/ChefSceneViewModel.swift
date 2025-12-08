b3fb0d47e6abe7e58aceaa18c545714acd064e02
= <=>
2025-12-08 14:34:12 -0300

    refactor: Translates english variables and parameters to portugues

--- oh-my-grill/oh-my-grill/Features/Chef/ChefSceneViewModel.swift
+++ oh-my-grill/oh-my-grill/Features/Chef/ChefSceneViewModel.swift
@@ -13,79 +13,79 @@ class ChefSceneViewModel {
 
     weak var scene: ChefScene!
 
-    let nomePrato = "prato"
+    let dishName = "dish"
 
-    var pratoNode: SKSpriteNode?
+    var dishNode: SKSpriteNode?
 
     private var cont = 0
 
-    let opcoes = ["burger-cheesed-shade", "tomato-sliced-shade", "lettuce-sliced-shade", "potato-cooked-shade", "bun-top-shade",  "bun-bottom-shade"]
+    let options = ["burger-cheesed-shade", "tomato-sliced-shade", "lettuce-sliced-shade", "potato-cooked-shade", "bun-top-shade",  "bun-bottom-shade"]
 
 
     func createRoundedDish(isPotato: Bool = false) {
 
         
 
-        let tamanhoPrato = isPotato ? CGSize(width: 100, height: 70) : CGSize(width: 140, height: 105)
+        let dishSize = isPotato ? CGSize(width: 100, height: 70) : CGSize(width: 140, height: 105)
 
-        let prato = SKSpriteNode(color: .lightGray, size: tamanhoPrato)
-
-        
-        let texturaRedonda = SKTexture(imageNamed: "dish")
-
-        prato.texture = texturaRedonda
-
-        prato.color = .white
-
-        prato.colorBlendFactor = 1.0
+        let dish = SKSpriteNode(color: .lightGray, size: dishSize)
 
         
+        let roundedTexture = SKTexture(imageNamed: "dish")
 
-        prato.position = isPotato ? CGPoint(x: (scene.frame.maxX - CGFloat(240)), y: (scene.frame.midY  - CGFloat(30))) : CGPoint(x: (scene.frame.maxX - CGFloat(120)), y: (scene.frame.midY - CGFloat(30)))
+        dish.texture = roundedTexture
 
-        prato.name = nomePrato
+        dish.color = .white
 
-        prato.zPosition = 0
+        dish.colorBlendFactor = 1.0
 
         
 
-        
+        dish.position = isPotato ? CGPoint(x: (scene.frame.maxX - CGFloat(240)), y: (scene.frame.midY  - CGFloat(30))) : CGPoint(x: (scene.frame.maxX - CGFloat(120)), y: (scene.frame.midY - CGFloat(30)))
 
-        prato.physicsBody = SKPhysicsBody(circleOfRadius: tamanhoPrato.width / 2)
+        dish.name = dishName
 
-        prato.physicsBody?.isDynamic = false
-
-        prato.physicsBody?.categoryBitMask = PhysicsCategory.dish
+        dish.zPosition = 0
 
         
 
-        scene.addChild(prato)
+        
 
-        self.pratoNode = prato
+        dish.physicsBody = SKPhysicsBody(circleOfRadius: dishSize.width / 2)
+
+        dish.physicsBody?.isDynamic = false
+
+        dish.physicsBody?.categoryBitMask = PhysicsCategory.dish
+
+        
+
+        scene.addChild(dish)
+
+        self.dishNode = dish
 
     }
     
-    func createIngredientComponent(nome: String, posicaoIngrediente: CGFloat) {
+    func createIngredientComponent(name: String, ingredientPosition: CGFloat) {
         
-        let tamanho = nome == "potato-cooked-shade" ? CGSize(width: 125  , height: 90) : CGSize(width: 90 , height: 90)
+        let size = name == "potato-cooked-shade" ? CGSize(width: 125  , height: 90) : CGSize(width: 90 , height: 90)
         
-        let ingrediente = SKSpriteNode(texture: SKTexture(imageNamed: nome), color: .clear, size: tamanho)
-        ingrediente.position = CGPoint(x: scene.frame.minX + posicaoIngrediente, y: scene.frame.minY + 50)
+        let ingredient = SKSpriteNode(texture: SKTexture(imageNamed: name), color: .clear, size: size)
+        ingredient.position = CGPoint(x: scene.frame.minX + ingredientPosition, y: scene.frame.minY + 50)
 
 
-        let contador = SKShapeNode(rectOf:  CGSize(width: 23, height: 23), cornerRadius: 20)
-        contador.position = CGPoint(x: scene.frame.minX + (posicaoIngrediente + 40), y: scene.frame.minY + 80)
-        contador.fillColor = .texasCherry
-        contador.strokeColor = .clear
+        let counter = SKShapeNode(rectOf:  CGSize(width: 23, height: 23), cornerRadius: 20)
+        counter.position = CGPoint(x: scene.frame.minX + (ingredientPosition + 40), y: scene.frame.minY + 80)
+        counter.fillColor = .texasCherry
+        counter.strokeColor = .clear
         
         let label = SKLabelNode(text: "0")
         label.fontName = "Toy Block Maestro"
         label.fontSize = 15
-        label.position = CGPoint(x: scene.frame.minX + (posicaoIngrediente + 40), y: scene.frame.minY + 73)
+        label.position = CGPoint(x: scene.frame.minX + (ingredientPosition + 40), y: scene.frame.minY + 73)
 
         scene.addChild(label)
-        scene.addChild(contador)
-        scene.addChild(ingrediente)
+        scene.addChild(counter)
+        scene.addChild(ingredient)
 
 
 
@@ -94,22 +94,22 @@ class ChefSceneViewModel {
     func createBench() {
         
         var startX = CGFloat(100)
-        let bancada = SKShapeNode(rectOf: CGSize(width: 676, height: 96), cornerRadius: 8)
+        let bench = SKShapeNode(rectOf: CGSize(width: 676, height: 96), cornerRadius: 8)
         
-        bancada.fillColor = .white
-        bancada.fillTexture = SKTexture.gradient(size: CGSize(width: 300, height: 100), colors: [.texasSalmon, .texasGrayGradient], startPoint: CGPoint(x: 0, y: 0), 
+        bench.fillColor = .white
+        bench.fillTexture = SKTexture.gradient(size: CGSize(width: 300, height: 100), colors: [.texasSalmon, .texasGrayGradient], startPoint: CGPoint(x: 0, y: 0), 
                                                  endPoint: CGPoint(x: 1, y: 3))
         
-        bancada.position = CGPoint(x: scene.frame.minX + 330, y: scene.frame.minY + 10)
+        bench.position = CGPoint(x: scene.frame.minX + 330, y: scene.frame.minY + 10)
         
-        for ingrediente in opcoes {
+        for ingrediente in options {
             
-            createIngredientComponent(nome: ingrediente, posicaoIngrediente: startX)
+            createIngredientComponent(name: ingrediente, ingredientPosition: startX)
             startX += 100
 
         }
 
-        scene.addChild(bancada)
+        scene.addChild(bench)
         
         
     }
