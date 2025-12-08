5bbad5a5ad79fb61e48fc6d72e0bf5b303b7252d
Maria <santellanoeduarda@gmail.com>
2025-12-08 14:59:18 -0300

    feat: Fry view

--- oh-my-grill/oh-my-grill/Components/OrderCard.swift
+++ oh-my-grill/oh-my-grill/Components/OrderCard.swift
@@ -58,7 +58,7 @@ struct OrderCard: View {
                 // FRIES
                 if order.meal.hasFries {
                     VStack(spacing: 2) {
-                        Image(.potato) // nome correto aqui
+                        Image(.potatoCooked)
                             .resizable()
                             .scaledToFit()
                             .frame(width: 34, height: 34)
