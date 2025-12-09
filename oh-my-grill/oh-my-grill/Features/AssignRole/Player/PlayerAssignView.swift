951f3eb187a4356fc15b6d4cffe53b71721a2f13
= <=>
2025-12-09 15:11:26 -0300

    refactor: Changes player assign view

--- oh-my-grill/oh-my-grill/Features/AssignRole/Player/PlayerAssignView.swift
+++ oh-my-grill/oh-my-grill/Features/AssignRole/Player/PlayerAssignView.swift
@@ -72,10 +72,7 @@ struct PlayerAssignView: View {
             .navigationBarBackButtonHidden(true)
             .fullScreenCover(isPresented: $nextView) {
                 if let session = vm.gameSession {
-//                    ChefView(vm: ChefViewModel(session: session))
-                    FryMinigameView(viewModel: FryMinigameViewModel(session: session))
-                    //GrillView(vm: GrillViewModel(session: session))
-                    //CutView(vm: CutViewModel(session: session))
+                    ChefView(vm: ChefViewModel(session: session))
                 }
             }
         }
